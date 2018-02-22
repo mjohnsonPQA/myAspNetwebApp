259d81ea1543fe77f1639d26570f1a575a74da4f
mjohnsonPQA <michelle.johnson@pqa.ca>
2018-02-22 08:43:49 -0400

    Add project files.

---
+++ myAspNetCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace myAspNetCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
